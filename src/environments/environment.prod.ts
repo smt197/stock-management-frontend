a390b657c5f22fa1dcc9203bdfd5bdadef6d71f8
smt197 <105449441+smt197@users.noreply.github.com>
2025-11-11 11:30:18 +0000

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: (globalThis as any).process?.env?.API_URL || 'https://votre-backend-render.onrender.com'
+  apiUrl: (globalThis as any).process?.env?.API_URL || 'https://stock-management-backend.onrender.com'
 };
