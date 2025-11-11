d30cab2b4b5c28bb81d30b2c025f50eebff2426a
smt197 <105449441+smt197@users.noreply.github.com>
2025-11-11 21:57:53 +0000

    update redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: (globalThis as any).process?.env?.API_URL || 'https://stock-management-backend.onrender.com'
+  apiUrl: (globalThis as any).process?.env?.API_URL || 'https://stock-management-backend-j33r.onrender.com/api/v1'
 };
